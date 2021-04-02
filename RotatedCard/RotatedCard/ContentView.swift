463f325e0d996382fced2a40956dc8031dacb22b
RutaTang <953259325@qq.com>
2021-04-02 05:08:05 -0600

    init RotatedCard Project

---
+++ RotatedCard/RotatedCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RotatedCard
+//
+//  Created by 唐伟智 on 4/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RotatedCard/RotatedCard/RotatedCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  RotatedCardApp.swift
+//  RotatedCard
+//
+//  Created by 唐伟智 on 4/2/21.
+//
+
+import SwiftUI
+
+@main
+struct RotatedCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
